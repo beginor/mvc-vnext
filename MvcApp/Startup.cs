e7b59a456701023cf6a5338bd1f03d2bf2a6c515
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-08-22 11:12:41 +0800

    init check in

---
+++ MvcApp/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.DependencyInjection;
+
+namespace MvcApp {
+
+    public class Startup {
+
+        public void Configure(IBuilder app) {
+            app.UseServices(services => {
+                services.AddMvc();
+            });
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default-api",
+                    template: "api/{controller}/{id?}"
+                );
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {
+                        controller = "Home",
+                        action = "Index"
+                    }
+                );
+            });
+        }
+    }
+}
