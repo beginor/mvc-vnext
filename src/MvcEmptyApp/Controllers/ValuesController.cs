ee8ab153c7050077fc3792d8911be3aab5fab270
beginor <beginor@hotmail.com.com>
2015-02-08 07:50:52 -0800

    beta2 style webapi

--- src/MvcEmptyApp/Controllers/ValuesController.cs
+++ src/MvcEmptyApp/Controllers/ValuesController.cs
@@ -1,31 +1,35 @@
 ﻿using Microsoft.AspNet.Mvc;
 using System.Collections.Generic;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace MvcApp.Controllers {
 
+    [Route("api/[controller]")]
     public class ValuesController : Controller {
 
         // GET api/values
+        [HttpGet("")]
         public IEnumerable<string> Get() {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
+        [HttpGet("{id:int}")]
         public string Get(int id) {
             return "value";
         }
 
-        // POST api/values
-        public void Post([FromBody]string value) {
+        // POST api/values/5
+        [HttpPost("{id:int}")]
+        public void Post(int id, [FromBody]string value) {
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value) {
+        // PUT api/values
+        [HttpPut("")]
+        public void Put([FromBody]string value) {
         }
 
         // DELETE api/values/5
+        [HttpDelete("{id:int}")]
         public void Delete(int id) {
         }
     }
